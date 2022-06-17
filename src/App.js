33d6f46bd504e8c061d23ee6ff1482d5096fd362
Natalya <chshebetun@gmail.com>
2022-06-17 14:15:06 -0700

    Added complete function for wave 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import { useState } from 'react';
 
 const TASKS = [
   {
@@ -16,13 +17,37 @@ const TASKS = [
 ];
 
 const App = () => {
+  const [tasks, setTasks] = useState(TASKS);
+
+  const completeTask = (id) => {
+    const tasksCopy = [...tasks];
+
+    for (let task of tasksCopy) {
+      if (task.id === id) {
+        task.isComplete = !task.isComplete;
+      }
+    }
+    // const tasksCopy = tasks.map((task) => {
+    //   console.log('id', id);
+    //   console.log('task id', task.id);
+    //   if (task.id === id) {
+    //     return !task.isComplete;
+    //   } else {
+    //     return task;
+    // //   }
+    // });
+    setTasks(tasksCopy);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>
+          {<TaskList tasks={TASKS} completeTaskCallback={completeTask} />}
+        </div>
       </main>
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  const [complete, setComplete] = useState(isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+const Task = ({ id, title, isComplete, completeTaskCallback }) => {
+  const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
 
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        onClick={() => completeTaskCallback(id)}
       >
         {title}
       </button>
+
       <button className="tasks__item__remove button">x</button>
     </li>
   );
@@ -24,6 +24,7 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  completeTaskCallback: PropTypes.func.isRequired,
 };
 
 export default Task;
